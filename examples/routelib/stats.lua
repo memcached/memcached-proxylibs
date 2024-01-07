cc00481035128bcbb14609e0954479c97c34cd0d
dormando <dormando@rydia.net>
2024-01-07 14:54:20 -0800

    routelib: example file that uses stats

---
+++ examples/routelib/stats.lua
@@ -0,0 +1,29 @@
+package.loaded["routelib"] = nil
+local s = require("routelib")
+verbose(true)
+debug(true)
+
+function config()
+    pools{
+        foo = {
+            servers = {
+                "127.0.0.1:11214",
+            }
+        },
+        baz = {
+            servers = {
+                "127.0.0.1:11215",
+            }
+        }
+    }
+
+    routes{
+        map = {
+            bar = route_latest{
+                children = { "foo", "baz" },
+                stats = true,
+            },
+        },
+    }
+end
+
