32f1281a58f79cc615eff75edaff6d899fadad9d
dormando <dormando@rydia.net>
2024-01-07 12:49:24 -0800

    routelib: magic stats counters + config refactor
    now have a fully working pre-parse stage before the route tree is passed
    to worker VM's. This gives a chance to do things like make custom stat
    names and get stat ID's assigned, which must be done from the
    configuration thread. Each route has a "route_name_conf" function called
    with its route tree data, and has access to the label and command from
    the tree for decorating stats and logs.
    
    You can do pretty much anything here: check custom globals/decorations
    for custom routes, etc.
    
    Then routes are passed to the worker VM's, which call the _next_ stage
    "route_name_start" which creates the function generator from the worker
    thread.
    
    The level of indirection pays off: greatly simplifies how many places
    configuration decoration/checks happen and gives us several
    opportunities to provide optimized request functions based on what options were
    requested by the user.
    
    Also adds the stats counter management stuff and an example.

--- lib/routelib/routelib.lua
+++ lib/routelib/routelib.lua
@@ -1,12 +1,29 @@
-local function module_defaults()
+local STATS_MAX <const> = 1024
+
+-- classes for typing.
+-- NOTE: metatable classes do not cross VM's, so they may only be used in the
+-- same VM they were assigned (pools or routes)
+local CommandMap = {}
+local RouteConf = {}
+
+local function module_defaults(old)
+    local stats = {
+        map = {},
+        freelist = {},
+        next_id = 1,
+    }
+    if old and old.stats then
+        stats = old.stats
+    end
     return {
         c_in = {
             pools = {},
             routes = {},
-        }
+        },
+        stats = stats,
     }
 end
-M = module_defaults()
+local M = module_defaults()
 
 -- https://stackoverflow.com/questions/9168058/how-to-dump-a-table-to-console
 -- should probably get a really nice one of these for the library instead.
@@ -89,6 +106,15 @@ function dsay(...)
     if M.is_debug then
         print(...)
     end
+end
+
+function cmdmap(t)
+    for k, v in pairs(t) do
+        if type(k) ~= "number" then
+            error("cmdmap keys must all be numeric")
+        end
+    end
+    return setmetatable(t, CommandMap)
 end
 
 --
@@ -213,9 +239,98 @@ local function pools_parse(a)
     return pools
 end
 
---
--- Worker thread configuration functions
---
+-- 1) walk keys looking for child*, recurse if RouteConfs are found
+-- 2) resolve and call route.f
+-- NOTE: this could be an actual function reference here...
+-- 3) return the response directly. the _conf() function should wrap the next
+-- stage itself.
+local function configure_route(r, ctx)
+    local route = r.a
+
+    -- first, recurse and resolve any children.
+    for k, v in pairs(route) do
+        -- try to not accidentally coerce a non-string key into a string!
+        if type(k) == "string" and string.find("^child", k) ~= nil then
+            if type(v) == "table" then
+                if (getmetatable(v) == RouteConf) then
+                    route[k] = configure_route(v, ctx)
+                else
+                    -- it is a table of children.
+                    for ck, cv in pairs(v) do
+                        if type(cv) == "table" and (getmetatable(cv) == RouteConf) then
+                            v[ck] = configure_route(cv, ctx)
+                        end
+                    end
+                end
+            end -- if "table"
+        end -- if "child"
+    end -- for(route)
+
+    local f = _G[r.f]
+    local ret = f(route, ctx)
+    if ret == nil then
+        error("route configure function failed to return anything: " .. r.f)
+    end
+    return ret
+end
+
+-- 1) walk the tree
+-- 2) for each entrypoint, look for child_* keys
+--  - check if metatable is RouteConf or not
+-- 3) for each child entry with a table, run configure_route()
+-- 4) resolve and run the .f function
+-- 5) take the result of that and pack it:
+--    { f = "route_name_start", a = t }
+-- routes that have stats should assign stats or global overrides in this conf
+-- stage.
+-- NOTE: we are editing the entries in-place
+local function configure_router(set)
+    -- create ctx object to hold label + command
+    local ctx = {
+        label = function(self)
+            return self._label
+        end,
+        cmd = function(self)
+            return self._cmd
+        end,
+    }
+
+    if set.map then
+        -- a prefix map
+        for mk, mv in pairs(set.map) do
+            ctx._label = mk
+            ctx._cmd = mcp.CMD_ANY_STORAGE
+            if (getmetatable(mv) == CommandMap) then
+                for cmk, cmv in pairs(mv) do
+                    if (getmetatable(cmv) ~= RouteConf) then
+                        error("bad entry in route table map")
+                    end
+                    -- replace the table entry
+                    ctx._cmd = cmk
+                    mv[cmk] = configure_route(cmv, ctx)
+                end
+            elseif (getmetatable(mv) == RouteConf) then
+                set.map[mk] = configure_route(mv, ctx)
+            else
+                error("unknown route map type")
+            end
+        end
+    else
+        -- a command map
+        ctx._label = "default"
+        -- walk set.cmap instead
+        for cmk, cmv in pairs(set.cmap) do
+            ctx._cmd = cmk
+            set.cmap[cmk] = configure_route(cmv, ctx)
+        end
+    end
+
+    if set.default then
+        ctx._label = "default"
+        ctx._cmd = mcp.CMD_ANY_STORAGE
+        set.default = configure_route(set.default, ctx)
+    end
+end
 
 -- TODO: allow a way to override which attach() happens for a router.
 -- by default we just do CMD_ANY_STORAGE
@@ -223,28 +338,88 @@ end
 -- config and routes sections.
 local function routes_parse(routes, pools)
     for tag, set in pairs(routes) do
-        set.is_prefix_map = true
-        local seen_string = false
-        local seen_number = false
-        for k, v in pairs(set.map) do
-            -- balk if we've seen both strings and numbers.
-            if type(k) == "number" then
-                seen_number = true
-                dsay("router for ", tag, "is not a command map")
-                set.is_prefix_map = false
-            elseif type(k) == "string" then
-                seen_string = true
-            else
-                error("router map keys must be strings or numbers")
-            end
+        if set.map and set.cmap then
+            error("cannot set both map and cmap for a router")
         end
 
-        if seen_string and seen_number then
-            error("router map keys must be only prefixes or only commands")
+        if set.map == nil and set.cmap == nil then
+            error("must pass map or cmap to a router")
         end
+
+        configure_router(set)
     end
+
     return { r = routes, p = pools }
 end
+
+-- route*() config functions can call this to get ids to use for stats counters
+function stats_get_id(name)
+    local st = M.stats
+
+    -- already have an ID for this stat name.
+    if st.map[name] then
+        return st.map[name]
+    end
+
+    -- same name was seen previously, refresh it for the new map and return.
+    if st.old_map and st.old_map[name] then
+        local id = st.old_map[name]
+        st.map[name] = id
+        return id
+    end
+
+    -- iterate the ID's
+    if st.next_id < STATS_MAX then
+        local id = st.next_id
+        st.next_id = id + 1
+        mcp.add_stat(id, name)
+        st.map[name] = id
+        return id
+    end
+
+    if #st.freelist == 0 then
+        error("max number of stat counters reached:", STATS_MAX)
+    end
+
+    -- pop a free id from the list
+    -- TODO: before uncommenting this code, the proxy needs to be able to
+    -- internally reset a counter when the name changes.
+    --local id = table.remove(st.freelist)
+    --mcp.add_stat(id, name)
+    --st.map[name] = id
+    --return id
+end
+
+-- _after_ pre-processing all route handlers, check if any existing stats
+-- counters are unused.
+-- if, across reloads, some stats counters disappear, we can reuse the IDs
+-- NOTE that this is a race, we cannot reuse an ID that was freed during the
+-- current load as they might be used by in-flight requests.
+-- We assume:
+-- 1) there's going to be some amount of time between reloads, usually more
+-- than enough time for in-flight requests to finish.
+-- 2) if this is ever not true, the harm is both rare and minimal.
+-- 3) this is probably improvable in the future once all the pre-API2 code is
+-- dropped and we can attach stats ID's to rctx's and do this
+-- recycling work internally in the proxy.
+function stats_turnover()
+    local st = M.stats
+
+    if st.old_map then
+        for k, v in pairs(st.old_map) do
+            if st.map[k] == nil then
+                -- key from old map not in new map, freelist the ID
+                table.insert(st.freelist, st.old_map[k])
+            end
+        end
+    end
+
+    st.old_map = st.map
+end
+
+--
+-- Worker thread configuration functions
+--
 
 -- re-wrap the arguments to create the function generator within a worker
 -- thread.
@@ -287,7 +462,7 @@ local function make_router(set, pools)
         end
     }
 
-    if set.is_prefix_map then
+    if set.map then
         -- create a new map with route entries resolved.
         for mk, mv in pairs(set.map) do
             -- duck type: if this map is a route or a command map
@@ -320,7 +495,7 @@ local function make_router(set, pools)
     else
         -- we're only routing based on the command, no prefix strings
         ctx._label = "default"
-        for cmk, cmv in pairs(set.map) do
+        for cmk, cmv in pairs(set.cmap) do
             ctx._cmd = cmk
             local fgen = make_route(cmv, ctx)
             if fgen == nil then
@@ -341,7 +516,7 @@ local function make_router(set, pools)
     end
 
     conf.map = map
-    if set.is_prefix_map then
+    if set.map then
         return mcp.router_new(conf)
     else
         return conf
@@ -349,8 +524,13 @@ local function make_router(set, pools)
 end
 
 --
+-- ----------------
 -- Loader functions
+-- ----------------
 --
+-- Contains the top level mcp_config_pools() and mcp_config_routes() handlers
+-- mcp_config_pools executes from the configuration thread
+-- mcp_config_routes executes from each worker thread
 
 function mcp_config_pools()
     dsay("mcp_config_pools: start")
@@ -368,7 +548,8 @@ function mcp_config_pools()
     local conf = routes_parse(M.c_in.routes, pools)
     -- Step 3) Reset global configuration
     dsay("mcp_config_pools: done")
-    M = module_defaults()
+    stats_turnover()
+    M = module_defaults(M)
 
     return conf
 end
@@ -429,8 +610,12 @@ function mcp_config_routes(c)
 end
 
 --
+-- ---------------------------------------------
 -- Configuration level route handler definitions
+-- ---------------------------------------------
 --
+-- Functions run from the configuration thread which pre-parse the provided
+-- route objects and do any required decoration or recursion
 
 -- route handlers on the configuration level are descriptors.
 -- actual functions need to be generated later, once passed to workers
@@ -438,23 +623,46 @@ end
 -- 2) return table with construction information:
 --    - function name (can't use func references since we're crossing VMs!)
 --    - config settings
-function route_allfastest(t)
+function route_allfastest_conf(t)
     -- TODO: validate arguments here.
     -- probably good to at least check that all children exist in the pools
     -- list.
     return { f = "route_allfastest_start", a = t }
 end
 
-function route_latest(t)
+function route_latest_conf(t, ctx)
+    if t.stats then
+        local name = ctx:label() .. "_retries"
+        if t.stats_name then
+            name = t.stats_name .. "_retries"
+        end
+        t.stats_id = stats_get_id(name)
+    end
+    if t.failover_count == nil then
+        t.failover_count = #t.children
+    end
     return { f = "route_latest_start", a = t }
 end
 
-function route_split(t)
+function route_split_conf(t)
     return { f = "route_split_start", a = t }
 end
 
+-- register global wrapper functions for collecting user input
+-- give the configuration a type so we can easily pick them out while walking
+-- the route map later.
+function register_route_handlers(t)
+    for _, name in pairs(t) do
+        _G["route_" .. name] = function(t)
+            return setmetatable({ f = "route_" .. name .. "_conf", a = t }, RouteConf)
+        end
+    end
+end
+
 --
+-- ---------------------------
 -- Worker level Route handlers
+-- ---------------------------
 --
 
 -- route process:
@@ -500,12 +708,48 @@ local function route_latest_f(rctx, arg)
     local count = arg.count
     local t = arg.t
 
-    return function(r)
-        for i=1, limit do
-            local r = rctx:enqueue_and_wait(r, t[i])
-            if r:ok() then
-                return r
+    -- example of returning an optimized function based on the requirements of
+    -- the function.
+    -- we could either specialize the generator function returned from
+    -- _start()
+    -- or do it here when the slots are generated.
+    if arg.stats_id then
+        local s = mcp.stat
+        local s_id = arg.stats_id
+        dsay("making latest route with a stats counter:" .. s_id)
+
+        return function(r)
+            local retry = false
+            local res = nil
+            for i=1, limit do
+                res = rctx:enqueue_and_wait(r, t[i])
+                if retry then
+                    -- increment the retries counter
+                    s(s_id, 1)
+                end
+                if res:ok() then
+                    return res
+                end
+                -- only increment the retries counter for an actual retry
+                retry = true
             end
+
+            -- didn't get what we want, return the final one.
+            return res
+        end
+    else
+        dsay("making latest route with no stats")
+        return function(r)
+            local res = nil
+            for i=1, limit do
+                local res = rctx:enqueue_and_wait(r, t[i])
+                if res:ok() then
+                    return res
+                end
+            end
+
+            -- didn't get what we want, return the final one.
+            return res
         end
     end
 end
@@ -520,7 +764,7 @@ function route_latest_start(a, ctx)
     -- only grab N entries.
     -- Not doing this _right now_ because I'm not confident children is an
     -- array or not.
-    for _, child in pairs(children) do
+    for _, child in pairs(a.children) do
         local c = ctx:get_child(child)
         table.insert(o.t, fgen:new_handle(c))
         o.c = o.c + 1
@@ -534,6 +778,7 @@ function route_latest_start(a, ctx)
     end
 
     o.limit = a.failover_count
+    o.stats_id = a.stats_id
 
     fgen:ready({ a = o, n = ctx:label(), f = route_latest_f })
     return fgen
@@ -566,3 +811,8 @@ end
 
 -- TODO: failover route
 
+register_route_handlers({
+    "latest",
+    "allfastest",
+    "split",
+})
