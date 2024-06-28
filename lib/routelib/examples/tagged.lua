4a6ebc674a772863d7c2c614d3fb229ba71eb985
dormando <dormando@rydia.net>
2024-06-28 10:44:47 -0700

    routelib: fix tagged example

--- lib/routelib/examples/tagged.lua
+++ lib/routelib/examples/tagged.lua
@@ -1,5 +1,6 @@
-verbose(true)
-debug(true)
+-- uncomment to debug
+--verbose(true)
+--debug(true)
 
 -- it's possible to have different route trees based on what port is being
 -- accessed to memcached.
@@ -18,30 +19,41 @@ pools{
     },
     bar = {
         backends = {
-            { host = "127.0.0.1", port = 11216, retrytimeout = 5 }
+            { host = "127.0.0.1", port = 11215, retrytimeout = 5 }
+        }
+    },
+    baz = {
+        backends = {
+            "127.0.0.1:11216",
         }
     },
 }
 
 -- no supplied tag makes this the "default" router.
 routes{
-    foo = route_allfastest{
-        children = { "foo" },
-    },
+    map = {
+        foo = route_allfastest{
+            children = { "foo" },
+        },
+    }
 }
 
 -- this route tree is only executed if a client is connected to port 12052
 routes{
     tag = "b",
-    foo = route_allfastest{
-        children = { "foo" },
-    },
+    map = {
+        foo = route_allfastest{
+            children = { "bar" },
+        },
+    }
 }
 
 -- this route tree is only executed if a client is connected to port 12053
 routes{
     tag = "cccc",
-    foo = route_allfastest{
-        children = { "foo" },
-    },
+    map = {
+        foo = route_allfastest{
+            children = { "baz" },
+        },
+    }
 }
