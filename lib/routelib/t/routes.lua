1a156b07855bc03ed30a8389f8c95c4c12bda486
s0m01n3 <sergei.mouravyov@walmart.com>
2025-01-15 15:11:59 -0800

    Added miss flag to route_allfastest
    Miss flag helps with using route_allfastest for executing parallel reads.
    Miss flag does not change write operations. For reads:
    - If miss==false, route_allfastest returns the first hit or miss, whatever comes first.
    - If miss==true,  route_allfastest waits for a hit and returns it. If no hit, it returns a miss (if any) or an error (if only errors returned by children).

--- lib/routelib/t/routes.lua
+++ lib/routelib/t/routes.lua
@@ -421,13 +421,23 @@ function TestAllFastest:testBasic()
     clearAll(p)
 end
 
-function TestAllFastest:testFirstFail()
+function TestAllFastest:testIgnoreMissAndErr()
     p:c_send("mg allfastest/b t\r\n")
     p:be_recv_c({1, 2, 3}, "all three got request")
     p:be_send(1, "SERVER_ERROR too many potatos\r\n")
-    p:be_send(2, "SERVER_ERROR something\r\n")
+    p:be_send(2, "EN\r\n") -- should ignore "miss" response when miss==true flag is set
     p:be_send(3, "HD t3\r\n")
     p:c_recv("HD t3\r\n")
+    clearAll(p)
+end
+
+function TestAllFastest:testReturnMiss()
+    p:c_send("mg allfastestnomiss/b t\r\n")
+    p:be_recv_c({1, 2, 3}, "all three got request")
+    p:be_send(1, "SERVER_ERROR too many potatos\r\n")
+    p:be_send(2, "EN\r\n") -- should return "miss" response when miss==false
+    p:be_send(3, "HD t3\r\n")
+    p:c_recv("EN\r\n")
     clearAll(p)
 end
 
@@ -441,13 +451,33 @@ function TestAllFastest:testLastErr()
     clearAll(p)
 end
 
-function TestAllFastest:testMiddleErr()
+function TestAllFastest:testMiddleHit()
     p:c_send("mg allfastest/d t\r\n")
     p:be_recv_c({1, 2, 3}, "all three got request")
     p:be_send(1, "SERVER_ERROR one\r\n")
     p:be_send(2, "HD t5\r\n")
-    p:be_send(3, "SERVER_ERROR final\r\n")
+    p:be_send(3, "EN\r\n")
     p:c_recv("HD t5\r\n")
+    clearAll(p)
+end
+
+function TestAllFastest:testMiddleMiss()
+    p:c_send("mg allfastest/d t\r\n")
+    p:be_recv_c({1, 2, 3}, "all three got request")
+    p:be_send(1, "SERVER_ERROR one\r\n")
+    p:be_send(2, "EN\r\n")
+    p:be_send(3, "SERVER_ERROR final\r\n")
+    p:c_recv("EN\r\n")
+    clearAll(p)
+end
+
+function TestAllFastest:testNoMissIgnoreLastHit()
+    p:c_send("mg allfastestnomiss/d t\r\n")
+    p:be_recv_c({1, 2, 3}, "all three got request")
+    p:be_send(1, "SERVER_ERROR one\r\n")
+    p:be_send(2, "EN\r\n") -- if miss==false, the first ok is returned ignoring subsequent hits
+    p:be_send(3, "HD t5\r\n")
+    p:c_recv("EN\r\n")
     clearAll(p)
 end
 
